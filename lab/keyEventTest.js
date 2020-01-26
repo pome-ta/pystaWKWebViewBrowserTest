4375f3e3d64a05ce2ba1abcf12e4250e8965e336
pome-ta <pometarou93@gmail.com>
2020-01-26 09:44:18 +0000

    mv keyevent

---
+++ lab/keyEventTest.js
@@ -0,0 +1,25 @@
+/* 押されたキーの確認 */
+// > 2020/01/26
+
+'use strict';
+eruda.init();
+
+let rsltUp = document.getElementById('rsltUp');
+let rsltDn = document.getElementById('rsltDn');
+//rslt.textContent = 'd';
+document.body.style.background = 'blue';
+
+// それを1秒後に戻す
+setTimeout(() => document.body.style.background = '', 500);
+
+
+window.addEventListener("keyup", function(e) {
+  let str = "up:key='" + e.key + "' | code='" + e.code + "'";
+  rsltUp.textContent = str;
+}, true);
+
+window.addEventListener("keydown", function(e) {
+  let str = "dn:key='" + e.key + "' | code='" + e.code + "'";
+  rsltDn.textContent = str;
+}, true);
+
